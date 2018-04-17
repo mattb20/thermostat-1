f2f2d741bf1ff338c4046255d1700e838999d85d
Patryk Pilecki <ppilecki@icloud.com>
2018-04-17 23:26:14 +0100

    minumum temperature + error when reached + test

--- jasmine-standalone-3.1.0/spec/thermostatSpec.js
+++ jasmine-standalone-3.1.0/spec/thermostatSpec.js
@@ -14,4 +14,11 @@ describe('Thermostat', function(){
     thermostat.down()
     expect(thermostat.temperature).toEqual(19)
   })
+  it('throw error when min temp reached', function(){
+    var thermostat = new Thermostat();
+    for (var i = 0; i <= 9; i++) {
+      thermostat.down()
+    }
+    expect( function(){ thermostat.down(); } ).toThrow(new Error("minimum temperature reached"));
+    })
 })
--- jasmine-standalone-3.1.0/src/thermostat.js
+++ jasmine-standalone-3.1.0/src/thermostat.js
@@ -5,5 +5,9 @@ Thermostat.prototype.up = function() {
   this.temperature += 1;
 }
 Thermostat.prototype.down = function() {
+  var minimumTemperature = 10
+  if (this.temperature <= minimumTemperature)
+  throw new Error("minimum temperature reached")
+  else
   this.temperature -= 1;
 }
