70d6fcf67553f05312f3a1d58404fc88faf347e8
Matthew <matt_burgoyne@live.co.uk>
2018-05-24 13:47:42 +0100

    added before each function so have a new thermostat for each test

--- jasmine-standalone-3.1.0/spec/thermostatSpec.js
+++ jasmine-standalone-3.1.0/spec/thermostatSpec.js
@@ -1,44 +1,39 @@
 describe('Thermostat', function(){
+  beforeEach(function() {
+    var thermotat = new Thermostat();
+  })
 
   it('starts with 20 dgree', function(){
-    var thermostat = new Thermostat();
     expect(thermostat.temperature).toEqual(20)
   })
   it('increase temperature', function(){
-    var thermostat = new Thermostat();
     thermostat.up()
     expect(thermostat.temperature).toEqual(21)
   })
   it('decrese temperature', function(){
-    var thermostat = new Thermostat();
     thermostat.down()
     expect(thermostat.temperature).toEqual(19)
   })
   it('throw error when min temp reached', function(){
-    var thermostat = new Thermostat();
     for (var i = 0; i <= 9; i++) {
       thermostat.down()
     }
     expect( function(){ thermostat.down(); } ).toThrow(new Error("minimum temperature reached"));
     })
     it('has a default maximum temperature 25(powerSaving on)', function(){
-      var thermostat = new Thermostat();
       expect(thermostat.maximumTemperature).toEqual(25)
     })
     it('changes to maximum temperature to 32 on powerSaving called(powerSaving off)', function(){
-      var thermostat = new Thermostat();
       thermostat.powerSaving()
       expect(thermostat.maximumTemperature).toEqual(32)
     })
     it('goes back to 25 if pressed againg 4 times cycle (powerSaving on)', function(){
-      var thermostat = new Thermostat();
       for (var i = 0; i <= 3; i++) {
       thermostat.powerSaving()
       }
       expect(thermostat.maximumTemperature).toEqual(25)
     })
     it('resets temperature', function(){
-      var thermostat = new Thermostat();
       for (var i = 0; i <= 3; i++) {
       thermostat.down()
       }
@@ -46,14 +41,12 @@ describe('Thermostat', function(){
       expect(thermostat.temperature).toEqual(20)
     })
     it('return low usage for 16', function(){
-      var thermostat = new Thermostat();
       for (var i = 0; i <= 3; i++) {
       thermostat.down()
       }
       expect(thermostat.currentUsage()).toEqual('low usage')
     })
     it('return high usage for 26', function(){
-      var thermostat = new Thermostat();
       thermostat.powerSaving()
       for (var i = 0; i <= 5; i++) {
       thermostat.up()
@@ -61,11 +54,9 @@ describe('Thermostat', function(){
       expect(thermostat.currentUsage()).toEqual('high usage')
     })
     it('return medium usage for 20', function(){
-      var thermostat = new Thermostat();
       expect(thermostat.currentUsage()).toEqual('medium usage')
     })
     it('throw error when max temp reached', function(){
-      var thermostat = new Thermostat();
       for (var i = 0; i <= 4; i++) {
         thermostat.up()
       }
